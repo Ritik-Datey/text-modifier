7455c31709836c72df73dcf53f657958d855fceb
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-07-02 10:53:36 +0530

    About section udated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ export default function NavBar(props) {
                 <a className="nav-link active" aria-current="page" href="/" style={{color:(textColor)}}>Home</a>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/" style={{color:(textColor)}}>About</a>
+                <a className="nav-link active" aria-current="page" href="/" data-bs-toggle="modal" data-bs-target="#exampleModal" style={{color:(textColor)}}>About</a>
                 </li>
             </ul>
             <div className="form-check form-switch">
@@ -45,6 +45,24 @@ export default function NavBar(props) {
         </div>
         </div>
         </nav>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">About Us</h1>
+                    {/* <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    {/* <button type="button" class="btn btn-primary">Save changes</button> */}
+                </div>
+                </div>
+            </div>
+            </div>
     </div>
   )
 }
