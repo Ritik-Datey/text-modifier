eb1cde673b11fe72dc85d7b4df50757355c58536
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-07-30 19:42:54 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ export default function NavBar(props) {
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
         <div className="container-fluid " style={{backgroundColor:`${mode}`}}>
-            <a className="navbar-brand" href="/" style={{color:(textColor)}}>{props.title}</a>
+            <a className="navbar-brand" href="https://ritik-datey.github.io/text-modifier/" style={{color:(textColor)}}>{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon "></span>
             </button>
